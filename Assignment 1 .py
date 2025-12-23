144deb1a464c69c986f0032a90ca4fbd86ed91cf
mann2029 <189124521+Mann2029@users.noreply.github.com>
2025-12-23 22:24:37 +0530

    Initial commit

---
+++ Assignment 1 .py
@@ -0,0 +1,30 @@
+# Task 1
+
+# Basic Arithmetic operations
+
+a1 = int(input("Enter first number: "))
+a2 = int(input("Enter second number: "))
+
+# Mathematical Operations
+addition = a1 + a2
+subtraction = a1 - a2
+multiplication = a1 * a2
+division = a1 / a2
+
+# Displaying the results
+print(f"Addition: {a1} + {a2} = {addition}")
+print(f"Subtraction: {a1} - {a2} = {subtraction}")
+print(f"Multiplication: {a1} * {a2} = {multiplication}")
+print(f"Division: {a1} / {a2} = {division}")
+
+
+# Task 2
+
+# Takes a user input first name and last name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Displaying the full name
+full_name = first_name + " " + last_name
+print(f"hello, {full_name}!, Welcome to Python program.")
